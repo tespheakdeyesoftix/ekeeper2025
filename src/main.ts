fb45b9ae9fb5b8fc875c15a0aef8b607d4bda460
tespheakdeyesoftix <pheakdey.micronet@gmail.com>
2025-02-18 10:22:59 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -52,7 +52,12 @@ import '@ionic/vue/css/display.css';
 
 /* @import '@ionic/vue/css/palettes/dark.always.css'; */
 /* @import '@ionic/vue/css/palettes/dark.class.css'; */
-import '@ionic/vue/css/palettes/dark.system.css';
+
+
+import '@ionic/vue/css/palettes/dark.always.css';
+import '@ionic/vue/css/palettes/dark.class.css'; 
+
+/* import '@ionic/vue/css/palettes/dark.system.css'; */
 
 /* Theme variables */
 import './theme/variables.css';
