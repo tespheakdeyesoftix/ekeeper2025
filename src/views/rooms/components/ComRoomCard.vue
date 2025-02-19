6b5eeb33202eab1973187e3c4384ebecbd8392de
Sengho825 <160992141+Honglek@users.noreply.github.com>
2025-02-19 14:56:26 +0700

    Update ComRoomCard.vue

--- src/views/rooms/components/ComRoomCard.vue
+++ src/views/rooms/components/ComRoomCard.vue
@@ -1,14 +1,28 @@
 <template>
-    <ion-card>
-      <ion-card-header color="primary" style="border-bottom: 1px solid gray; padding: 5px;">
-        <ion-card-title style="text-align: center;">A 102</ion-card-title>
-      </ion-card-header>
-  
-      <ion-card-content style="padding: 3px; font-size: 12;">
+  <ion-card>
+    <ion-card-header color="primary" style="padding: 5px">
+      <ion-card-title style="text-align: center">A 102</ion-card-title>
+    </ion-card-header>
+
+    <ion-card-content style="padding: 0px">
+      <div style="padding: 5px">
         <ion-label>Pokémon Yel</ion-label>
-      </ion-card-content>
-    </ion-card>
-  </template>
-  
-  <script lang="ts" setup>
-  </script>
+        <div style="display: flex; align-items: center">
+          <ion-icon :icon="bed"></ion-icon>
+          <h6>1/2</h6>
+        </div>
+        <h6>Deposit Guest</h6>
+      </div>
+      <div style="border-top: 1px solid gray; padding: 5px; display: flex;  gap: 5px;">
+        <ion-icon :icon="bed" style="background-color: red; padding: 5px; border-radius: 100%;"></ion-icon>
+        <ion-icon :icon="bed" style="background-color: green; padding: 5px; border-radius: 100%;"></ion-icon>
+      </div>
+    </ion-card-content>
+  </ion-card>
+</template>
+
+<script lang="ts" setup>
+import { bed } from "ionicons/icons";
+</script>
+
+<style scoped></style>
