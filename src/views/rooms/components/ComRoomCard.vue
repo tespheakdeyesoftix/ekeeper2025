44f6a3b006bbec23537550ec4175859a856d6150
Sengho825 <160992141+Honglek@users.noreply.github.com>
2025-02-20 08:31:58 +0700

    Update ComRoomCard.vue

--- src/views/rooms/components/ComRoomCard.vue
+++ src/views/rooms/components/ComRoomCard.vue
@@ -1,28 +1,72 @@
 <template>
-    <ion-card v-longPress="onLongPress" button>
-      <ion-ripple-effect></ion-ripple-effect>
-      <ion-card-header color="primary" style="border-bottom: 1px solid gray; padding: 5px;">
-        <ion-card-title style="text-align: center;">{{ data?.room_number }} - {{ data?.floor_alias }}</ion-card-title>
-      </ion-card-header>
-  
-      <ion-card-content style="padding: 3px; font-size: 12;">
-        <ion-label>Pokémon Yel</ion-label>
-      </ion-card-content>
-    </ion-card>
-  </template>
-  
-  <script lang="ts" setup>
-  const props = defineProps({
-    data:Object
-  })
+  <ion-card v-longPress="onLongPress" button>
+    <ion-ripple-effect></ion-ripple-effect>
+    <ion-card-header
+      color="primary"
+      style="padding: 5px"
+      :style="{
+        padding: '5px',
+        backgroundColor:
+          data?.room_status === 'Room Block'
+            ? '#ffffff'
+            : data?.reservation_status_color || '#eae3e3',
+      }"
+    >
+      <ion-card-title style="text-align: center">
+        {{ data?.room_number }} - {{ data?.floor_alias }}
+        </ion-card-title>
+    </ion-card-header>
 
-  const emit = defineEmits()
+    <ion-card-content style="padding: 0px">
+      <div style="padding: 5px; height: 80px;" >
+        <ion-label  class="truncate" >{{ data?.guest_name }}</ion-label>
+        <div style="display: flex; align-items: center">
+          <ion-icon :icon="bed"></ion-icon>
+          <h6>{{ data?.adult }}/{{ data?.child }}</h6>
+        </div>
+        <h6>Deposit Guest</h6>
+      </div>
+      <!-- icon -->
+      <div
+        style="
+          border-top: 1px solid gray;
+          padding: 5px;
+          display: flex;
+          gap: 5px;
+        "
+      >
+        <ion-icon
+          :icon="bed"
+          style="background-color: red; padding: 5px; border-radius: 100%"
+        ></ion-icon>
+        <ion-icon
+          :icon="bed"
+          style="background-color: green; padding: 5px; border-radius: 100%"
+        ></ion-icon>
+      </div>
+    </ion-card-content>
+  </ion-card>
+</template>
 
-  function onLongPress(){
- 
-    emit("onLongPress",props.data)
-  }
+<script lang="ts" setup>
+import { bed } from "ionicons/icons";
+const props = defineProps({
+  data: Object,
+});
 
+const emit = defineEmits();
 
+function onLongPress() {
+  emit("onLongPress", props.data);
+}
+</script>
+<style>
+.truncate {
+  display: block;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;  
+  max-width: 100%;         
+}
 
-  </script>
+</style>
