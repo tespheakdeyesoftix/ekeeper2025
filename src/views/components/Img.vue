43656623e96ad93e5905ac7e5234780b2665af72
MeyGB <sopmey002@gmail.com>
2025-02-24 10:46:04 +0700

    update

--- src/views/components/Img.vue
+++ src/views/components/Img.vue
@@ -1,6 +1,6 @@
 <template>
 
-      <img   :src="imgData" :width="width==0?undefined:width" :height="height==0?undefined:height" />
+      <img   :src="imgData" :width="width==0?undefined:width" :height="height==0?undefined:height"/>
   
   </template>
 <script setup>
@@ -82,3 +82,4 @@ onMounted(async () => {
   getImage()
 });
 </script>
+ 
