ab7d898fed5c561c117f6ad604aba4e92e46d677
MeyGB <sopmey002@gmail.com>
2025-03-01 16:39:05 +0700

    update

--- src/views/task/Task.vue
+++ src/views/task/Task.vue
@@ -2,11 +2,24 @@
   <ion-page>
     <AppBar>{{ t("Task") }}</AppBar>
     <ion-content class="ion-padding">
-
-     <ComTaskKpiCard :label="t('Ongoing')" color="primary" :icon="timeOutline">{{ summaryData?.ongoing }}</ComTaskKpiCard>
-     <ComTaskKpiCard :label="t('Complete')" color="success" :icon="timeOutline">{{ summaryData?.complete }}</ComTaskKpiCard>
-     <ComTaskKpiCard :label="t('In Progress')" color="warning" :icon="timeOutline">{{ summaryData?.in_progress }}</ComTaskKpiCard>
-     <ComTaskKpiCard :label="t('Cancelled')" color="danger" :icon="timeOutline">{{ summaryData?.cancelled }}</ComTaskKpiCard>
+    <ion-grid>
+      <ion-row>
+        <ion-col>
+          <ComTaskKpiCard :label="t('Ongoing')" color="primary" :icon="refreshOutline">{{ summaryData?.ongoing }}</ComTaskKpiCard>
+        </ion-col>
+        <ion-col>
+          <ComTaskKpiCard :label="t('Complete')" color="success" :icon="checkmarkCircleOutline">{{ summaryData?.complete }}</ComTaskKpiCard>
+        </ion-col>
+      </ion-row>
+      <ion-row>
+        <ion-col>
+        <ComTaskKpiCard :label="t('In Progress')" color="warning" :icon="timeOutline">{{ summaryData?.in_progress }}</ComTaskKpiCard>
+        </ion-col>
+        <ion-col>
+        <ComTaskKpiCard :label="t('Cancelled')" color="danger" :icon="closeCircleOutline">{{ summaryData?.cancelled }}</ComTaskKpiCard>
+        </ion-col>
+      </ion-row>
+    </ion-grid>
 
       <ion-button expand="block" router-link="/all-task">{{t("All Task")}}</ion-button>
 
@@ -18,7 +31,13 @@
       
       docType="Work Order"
       :fields="['name','work_order_type','location','description','photo','work_order_status']"
-      :filters="[['_assign', 'like', `%${currentUser.name}%`],['property','like',  `%${currentProperty.property_name}%`],['workorder_date','<=', currentWorkingDate],['due_date','>=', currentWorkingDate]]"
+      :filters="[
+        ['_assign', 'like', `%${currentUser.name}%`],
+        ['property','like',  `%${currentProperty.property_name}%`],
+        ['workorder_date','<=', currentWorkingDate],
+        ['due_date','>=', currentWorkingDate],
+        ['work_order_status', 'in', ['New','In Progress']]
+        ]"
       :showSearchBar="false" 
       @onRefresh="onRefresh"
       > 
@@ -32,13 +51,18 @@
         </template>
       </DocList>
     </ion-content>
+    <ion-fab vertical="bottom" horizontal="end" @click="handleFabClick">
+      <ion-fab-button color="success">
+        <ion-icon :icon="add"/>
+      </ion-fab-button>
+    </ion-fab>
   </ion-page> 
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
 import { useIonRouter } from "@ionic/vue";
-import { refreshOutline, timeOutline, documentOutline, closeOutline, gitCompareOutline, key } from 'ionicons/icons';
+import { refreshOutline, timeOutline, closeCircleOutline, checkmarkCircleOutline, gitCompareOutline, key,add } from 'ionicons/icons';
 import ComTaskCard from "./components/ComTaskCard.vue";
 import { useAuth } from "@/hooks/useAuth";
 import { useApp } from "@/hooks/useApp";
@@ -51,6 +75,11 @@ const {currentUser} = useAuth()
 const {currentProperty,currentWorkingDate} = useApp()
 
 const t = window.t;
+
+function handleFabClick() {
+  alert(123)
+}
+
 const summaryData=ref()
 function onViewTaskDetail(task: any) {
   ionRouter.navigate('/task-detail/' + task.name, 'forward', 'push');
