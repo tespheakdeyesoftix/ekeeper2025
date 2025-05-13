1b2d0606b464660c56e6bac1eb09c101eb800c18
tespheakdeyesoftix <pheakdey.micronet@gmail.com>
2025-05-13 14:30:13 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <AppBar>{{ t("Home") }}</AppBar>
     <ion-content :fullscreen="true" class="ion-padding">
       <ion-button routerLink="/product-list">Product List</ion-button>
-      <ion-button routerLink="/add-product">Add Product</ion-button>
+  
     </ion-content>
   </ion-page>
 </template>
